9cc2db0e4a32d1ed26823d96476f579e259c9417
wangweiwei <wangweiwei@ugreen.com>
2025-06-11 11:32:57 +0800

    feat: 添加字符和数字的常量

---
+++ src/main/java/com/weiwei/wang/common/constant/CharacterConstant.java
@@ -0,0 +1,42 @@
+package com.weiwei.wang.common.constant;
+
+/**
+ * 字符常量
+ */
+public final class CharacterConstant {
+
+    /**
+     * 逗号
+     */
+    public static final String COMMA = ",";
+
+    /**
+     * 下划线
+     */
+    public static final String UNDERLINE = "_";
+
+    /**
+     * 中划线
+     */
+    public static final String LINE_THROUGH = "-";
+
+    /**
+     * 空字符串
+     */
+    public static final String EMPTY_STRING = "";
+
+    /**
+     * 百分号
+     */
+    public static final String PERCENTAGE_SIGN = "%";
+
+    /**
+     * limit
+     */
+    public static final String LIMIT = "limit";
+
+    /**
+     * limit 1
+     */
+    public static final String LIMIT_ONE = " limit 1";
+}
---
+++ src/main/java/com/weiwei/wang/common/constant/NumberConstant.java
@@ -0,0 +1,32 @@
+package com.weiwei.wang.common.constant;
+
+/**
+ * 数字常量定义
+ */
+public final class NumberConstant {
+
+    public static final int ZERO_INT = 0;
+    public static final int ONE_INT = 1;
+    public static final int TWO_INT = 2;
+    public static final int THREE_INT = 3;
+    public static final int FOUR_INT = 4;
+    public static final int FIVE_INT = 5;
+    public static final int SIX_INT = 6;
+    public static final int SEVEN_INT = 7;
+    public static final int EIGHT_INT = 8;
+    public static final int NINE_INT = 9;
+    public static final int TEN_INT = 10;
+
+    public static final long ZERO_LONG = 0L;
+    public static final long ONE_LONG = 1L;
+    public static final long TWO_LONG = 2L;
+    public static final long THREE_LONG = 3L;
+    public static final long FOUR_LONG = 4L;
+    public static final long FIVE_LONG = 5L;
+    public static final long SIX_LONG = 6L;
+    public static final long SEVEN_LONG = 7L;
+    public static final long EIGHT_LONG = 8L;
+    public static final long NINE_LONG = 9L;
+    public static final long TEN_LONG = 10L;
+
+}
